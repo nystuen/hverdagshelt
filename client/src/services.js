2fd99adeccb09f8a09bce5526e327082ca0dbf02
Ilia <ilia.rad.saadat@hotmail.com>
2019-01-07 13:02:03 +0100

    Setting up project

--- client/src/services.js
+++ client/src/services.js
@@ -2,24 +2,4 @@
 import axios from 'axios';
 axios.interceptors.response.use(response => response.data);
 
-class Student {
-  id: number;
-  firstName: string;
-  lastName: string;
-  email: string;
-}
 
-class StudentService {
-  getStudents(): Promise<Student[]> {
-    return axios.get('/students');
-  }
-
-  getStudent(id: number): Promise<Student> {
-    return axios.get('/students/' + id);
-  }
-
-  updateStudent(student: Student): Promise<void> {
-    return axios.put('/students', student);
-  }
-}
-export let studentService = new StudentService();
