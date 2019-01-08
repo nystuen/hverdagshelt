2fc029b250acae8e3c727f8334bc73abfa83fd0d
Albert Ohrem Larsen <albertla@stud.ntnu.no>
2019-01-08 09:17:18 +0100

    Deleted unneccesary import

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,6 @@ import express from 'express';
 import path from 'path';
 import reload from 'reload';
 import fs from 'fs';
-import { Students } from './models.js';
 
 type Request = express$Request;
 type Response = express$Response;
@@ -51,7 +50,8 @@ if (process.env.NODE_ENV !== 'production') {
 // The listen promise can be used to wait for the web server to start (for instance in your tests)
 export let listen = new Promise<void>((resolve, reject) => {
   app.listen(3000, error => {
-    if (error) reject(error.message);
+      console.log(error)
+      if (error) reject(error.message);
     console.log('Server started');
     resolve();
   });
