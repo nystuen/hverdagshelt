5886167c86496849baf8fe6fcb644eb99e359910
Magnus Revheim Martinsen <magnusrm.stud.ntnu.no>
2019-01-08 15:31:40 +0100

    Added test for userDao

---
+++ server/tests/sqlFiles/createTestData.sql
@@ -0,0 +1,20 @@
+INSERT INTO user (mail, firstName, lastName, typeName, phone, password, points, countyId, active)
+VALUES ('halo@gutta.no', 'per', 'sandberg', 'privat', '34985687', 'hei123', 0, 1, 1);
+INSERT INTO user (mail, firstName, lastName, typeName, phone, password, points, countyId, active)
+VALUES ('1@gutta.no', 'per', 'sandberg', 'privat', '34985687', 'hei123', 0, 1, 1);
+INSERT INTO user (mail, firstName, lastName, typeName, phone, password, points, countyId, active)
+VALUES ('halo2@gutta.no', 'per', 'sandberg', 'privat', '34985687', 'hei123', 0, 1, 1);
+INSERT INTO user (mail, firstName, lastName, typeName, phone, password, points, countyId, active)
+VALUES ('halo3@gutta.no', 'per', 'sandberg', 'privat', '34985687', 'hei123', 0, 1, 1);
+INSERT INTO user (mail, firstName, lastName, typeName, phone, password, points, countyId, active)
+VALUES ('halo4@gutta.no', 'per', 'sandberg', 'privat', '34985687', 'hei123', 0, 1, 1);
+INSERT INTO user (mail, firstName, lastName, typeName, phone, password, points, countyId, active)
+VALUES ('halo5@gutta.no', 'per', 'sandberg', 'privat', '34985687', 'hei123', 0, 1, 1);
+INSERT INTO user (mail, firstName, lastName, typeName, phone, password, points, countyId, active)
+VALUES ('halo6@gutta.no', 'per', 'sandberg', 'privat', '34985687', 'hei123', 0, 1, 1);
+INSERT INTO user (mail, firstName, lastName, typeName, phone, password, points, countyId, active)
+VALUES ('halo7@gutta.no', 'per', 'sandberg', 'privat', '34985687', 'hei123', 0, 1, 1);
+INSERT INTO user (mail, firstName, lastName, typeName, phone, password, points, countyId, active)
+VALUES ('halo8@gutta.no', 'per', 'sandberg', 'privat', '34985687', 'hei123', 0, 1, 1);
+INSERT INTO user (mail, firstName, lastName, typeName, phone, password, points, countyId, active)
+VALUES ('halo9@gutta.no', 'per', 'sandberg', 'privat', '34985687', 'hei123', 0, 1, 1);
