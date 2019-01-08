58bfaac92617ae2d70ec28411c0244bf979bc42a
Ilia <ilia.rad.saadat@hotmail.com>
2019-01-08 14:04:58 +0100

    Merge branches 'feature/user_registration/backend' and 'feature/user_registration/frontend' of https://gitlab.stud.idi.ntnu.no/sindretn/hverdagshelt into feature/user_registration/frontend
    # Conflicts:
    #	.gitignore

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -3,11 +3,11 @@
 let bodyParser = require('body-parser');
 let urlencodedParser = bodyParser.urlencoded({extended: false});
 
-module.exports = function (app: Object, postDao: Object) {
+module.exports = function (app: Object, userDao: Object) {
 
     app.post('/add_user', urlencodedParser, (req, res) => {
         console.log('got post request from add_user');
-        postDao.addUser(req.body, (status, data) => {
+        userDao.addUser(req.body, (status, data) => {
             res.status(status);
             res.json(data);
         })
