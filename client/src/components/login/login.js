786f5fb7c38bc2f6caf699abceb9993821a7b308
Ilia <ilia.rad.saadat@hotmail.com>
2019-01-09 10:57:32 +0100

    Commit before pull

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -2,7 +2,7 @@
 
 import ReactDOM from 'react-dom';
 import * as React from 'react';
-import { Component } from 'react-simplified';
+import { Component } from 'react';
 import {User} from "../../classTypes";
 
 export class Login extends Component{
