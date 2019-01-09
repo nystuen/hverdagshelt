30a791860d27362d3cf9df4c60118a92b176d425
Magnus Revheim Martinsen <magnusrm.stud.ntnu.no>
2019-01-09 15:47:34 +0100

    created view MinSide

--- client/src/views/MinSide/MinSide.js
+++ client/src/views/MinSide/MinSide.js
@@ -7,18 +7,18 @@ import {Container, Col, Row} from "reactstrap";
 export class MinSide extends React.Component {
     render() {
         return (
-                <Container>
-                        <Row>
-                            <Col xs="3">
-                                <div>
-                                    <MinSideMenu type='privat'/>
-                                </div>
-                            </Col>
-                            <Col xs="7">
-                                <h1>Min Side</h1>
-                            </Col>
-                        </Row>
-                </Container>
+            <Container>
+                <Row>
+                    <Col xs="3">
+                        <div>
+                            <MinSideMenu type='privat'/>
+                        </div>
+                    </Col>
+                    <Col xs="7">
+                        <h1>Min Side</h1>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
