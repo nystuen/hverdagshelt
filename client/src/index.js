af7082d26f3e2370e06892050ec53391ab88b8a0
Magnus Revheim Martinsen <magnusrm.stud.ntnu.no>
2019-01-09 12:07:09 +0100

    added NavbarMenu

--- client/src/index.js
+++ client/src/index.js
@@ -2,29 +2,31 @@
 
 import ReactDOM from 'react-dom';
 import * as React from 'react';
-import { Component } from 'react-simplified';
-import { HashRouter, Route, NavLink } from 'react-router-dom';
-import { Alert } from './widgets';
+import {Component} from 'react-simplified';
+import {HashRouter, Route, NavLink} from 'react-router-dom';
+import {Alert} from './widgets';
+import {NavbarMenu} from "./components/NavbarMenu/NavbarMenu";
 
 // Reload application when not in production environment
 if (process.env.NODE_ENV !== 'production') {
-  let script = document.createElement('script');
-  script.src = '/reload/reload.js';
-  if (document.body) document.body.appendChild(script);
+    let script = document.createElement('script');
+    script.src = '/reload/reload.js';
+    if (document.body) document.body.appendChild(script);
 }
 
 import createHashHistory from 'history/createHashHistory';
-const history = createHashHistory(); // Use history.push(...) to programmatically change path, for instance after successfully saving a student
 
+const history = createHashHistory(); // Use history.push(...) to programmatically change path, for instance after successfully saving a student
 
 
 const root = document.getElementById('root');
 if (root)
-  ReactDOM.render(
-    <HashRouter>
-      <div>
-        <Alert />
-      </div>
-    </HashRouter>,
-    root
-  );
+    ReactDOM.render(
+        <HashRouter>
+            <div>
+                <NavbarMenu/>
+                <Alert/>
+            </div>
+        </HashRouter>,
+        root
+    );
