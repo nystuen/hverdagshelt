6f02c6c8c997d5532de50fe04021b70965aefcca
Patrick Thorkildsen <patrickt@stud.ntnu.no>
2019-01-09 14:22:34 +0100

    Added simple form to register a new user

--- client/src/index.js
+++ client/src/index.js
@@ -1,29 +1,29 @@
 // @flow
 
-import ReactDOM from 'react-dom';
-import * as React from 'react';
-import { Component } from 'react-simplified';
-import { HashRouter, Route, NavLink } from 'react-router-dom';
-import { Alert } from './widgets';
+import ReactDOM from "react-dom";
+import * as React from "react";
+import { Component } from "react-simplified";
+import { HashRouter, Route, NavLink } from "react-router-dom";
+import { Alert } from "./widgets";
+import { RegisterIssue } from "./components/RegisterIssue.js";
 
 // Reload application when not in production environment
-if (process.env.NODE_ENV !== 'production') {
-  let script = document.createElement('script');
-  script.src = '/reload/reload.js';
+if (process.env.NODE_ENV !== "production") {
+  let script = document.createElement("script");
+  script.src = "/reload/reload.js";
   if (document.body) document.body.appendChild(script);
 }
 
-import createHashHistory from 'history/createHashHistory';
+import createHashHistory from "history/createHashHistory";
 const history = createHashHistory(); // Use history.push(...) to programmatically change path, for instance after successfully saving a student
 
-
-
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 if (root)
   ReactDOM.render(
     <HashRouter>
       <div>
         <Alert />
+        <Route exact path="/registerIssue" component={RegisterIssue} />
       </div>
     </HashRouter>,
     root
