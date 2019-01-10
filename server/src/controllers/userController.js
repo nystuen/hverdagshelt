c092ba9ddb35be4a5ababb761338494e597540fb
Magnus Revheim Martinsen <magnusrm.stud.ntnu.no>
2019-01-10 13:18:18 +0100

    added webtoken for login

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,5 +1,6 @@
 // @flow
 
+let jwt = require("jsonwebtoken");
 let bodyParser = require('body-parser');
 let urlencodedParser = bodyParser.urlencoded({extended: false});
 
@@ -21,9 +22,25 @@ module.exports = function (app: Object, userDao: Object) {
         });
     });
 
-    app.get('/get_user/:userMail', urlencodedParser, (req, res) => {
+    app.post('/login/', urlencodedParser, (req, res) => {
+        console.log('got login request');
+        console.log(req.body);
+        if (req.body) {
+            console.log("Brukernavn & passord ok");
+            let token = jwt.sign({ userEmail: req.body.userEmail }, {
+                expiresIn: 2419200
+            });
+            res.json({ jwt: token });
+        } else {
+            console.log("Brukernavn & passord IKKE ok");
+            res.status(401);
+            res.json({ error: "Not authorized" });
+        }
+    });
+
+    app.get('/get_user/:email', urlencodedParser, (req, res) => {
         console.log('got request from get_user');
-        userDao.getUser(req.params.userMail, (status, data) => {
+        userDao.getUser(req.params.email, (status, data) => {
             res.status(status);
             res.json(data);
         });
