179d6406e4721ea5409bab44192f244c44d49b20
annabesa <annabesa@stud.ntnu.no>
2019-01-18 11:20:40 +0100

    testsetup works

--- server/tests/issueDaoTest.js
+++ server/tests/issueDaoTest.js
@@ -1,75 +1,149 @@
-import mysql from "mysql2";
+//import mysql from "mysql2";
+
+let mysql = require("mysql");
 import runsqlfile from './runSqlFile';
 import {IssueDao} from "../src/daos/issueDao";
+import {UserDao} from "../src/daos/userDao";
+
+
 
 // GitLab CI Pool
 let pool = mysql.createPool({
-    connectionLimit: 1,
-    host: "mysql",
-    user: "root",
-    password: "abc123",
-    database: "testdb",
-    debug: false,
-    multipleStatements: true
+  connectionLimit: 2,
+  host: "mysql.stud.iie.ntnu.no",
+  user: "aadneny",
+  password: "W9d7XVXV",
+  database: "aadneny",
+  debug: false,
+  multipleStatements: true
 });
 
 let issueDao = new IssueDao(pool);
-
+let userDao = new UserDao(pool);
 
 beforeAll(done => {
-    runsqlfile("tests/sqlFiles/createTables.sql", pool, () => {
-        runsqlfile("tests/sqlFiles/createTestData.sql", pool, done);
-    });
+  runsqlfile("tests/sqlFiles/createTables.sql", pool, () => {
+    runsqlfile("tests/sqlFiles/createTestData.sql", pool, done);
+  });
 });
 
 afterAll(() => {
-    pool.end();
+  pool.end();
+});
+
+
+//ISSUE-TESTING
+//-----------------------------------------------------------------
+test("check if  issue is exist", done => {
+  function callback(status, data) {
+    console.log(
+      "Test callback: status=" + status + ", data=" + JSON.stringify(data)
+    );
+    expect(data[0].userMail).toBe("ola@usermail.com");
+    done();
+  }
+  issueDao.getOneIssue(1, callback);
 });
 
 
 test("Add a issue to database", done => {
-    function callback(status, data) {
-        console.log(
-            "Test callback: status=" + status + ", data=" + JSON.stringify(data)
-        );
-        expect(data.affectedRows).toBeGreaterThanOrEqual(1);
-        done();
-    }
+  function callback(status, data) {
+    console.log(
+      "Test callback: status=" + status + ", data=" + JSON.stringify(data)
+    );
+    expect(data.affectedRows).toBeGreaterThanOrEqual(1);
+    done();
+  }
 
 
-    let post = {
-        userMail: 'petter@usermail.com',
-        latitude: 23.234,
-        longitude: 40.91,
-        text: 'TEST',
-        pic: 'TEST ',
-        date: 'TEST ',
-        statusName: 'In progress',
-        categoryId: 1,
-        countyId: 1,
-        active: 1
-    };
+  let post = {
+    userMail: 'ola@usermail.com',
+    latitude: 23.234,
+    longitude: 40.91,
+    address: 'test adresse',
+    text: 'TEST',
+    pic: 'TEST',
+    date: 'TEST',
+    statusName: 'In progress',
+    categoryId: 1,
+    categoryLevel: 1,
+    countyId: 1,
+    active: 1
+  };
 
-    issueDao.addIssue(post, callback);
+  issueDao.addIssue(post, callback);
 
 });
 
-test("check if added issue is added", done => {
-    function callback(status, data) {
-        console.log(
-            "Test callback: status=" + status + ", data=" + JSON.stringify(data)
-        );
-        expect(data[0].userMail).toBe("petter@usermail.com");
-        expect(data[0].latitude).toBe(23.234);
-        expect(data[0].longitude).toBe(40.91);
-        expect(data[0].text).toBe('TEST');
-        expect(data[0].pic).toBe('TEST');
-        expect(data[0].date).toBe('TEST');
-        expect(data[0].statusName).toBe('In progress');
-        expect(data[0].categoryId).toBe(1);
-        expect(data[0].countyId).toBe(1);
-        expect(data[0].active).toBe(1);
-        done();
-    }
-    issueDao.getOneIssue(3, callback);
+
+test("check if ola added an issue", done => {
+  function callback(status, data) {
+    console.log(
+      "Test callback: status=" + status + ", data=" + JSON.stringify(data)
+    );
+    expect(data[0].userMail).toBe("ola@usermail.com");
+    expect(data[0].latitude).toBe(23.234);
+    expect(data[0].longitude).toBe(40.91);
+    expect(data[0].address).toBe('test adresse');
+    expect(data[0].text).toBe('TEST');
+    expect(data[0].pic).toBe('TEST');
+    expect(data[0].date).toBe('TEST');
+    expect(data[0].statusName).toBe('In progress');
+    expect(data[0].categoryId).toBe(1);
+    expect(data[0].categoryLevel).toBe(1);
+    expect(data[0].countyId).toBe(1);
+    expect(data[0].active).toBe(1);
+
+    done();
+  }
+  issueDao.getOneIssue(3, callback);
 });
+
+
+//USER-TESTING
+//-----------------------------------------------------------------
+
+test("Add a user to database", done => {
+  function callback(status, data) {
+    console.log(
+      "Test callback: status=" + status + ", data=" + JSON.stringify(data)
+    );
+    expect(data.affectedRows).toBeGreaterThanOrEqual(1);
+    done();
+  }
+
+
+  let post = {
+    mail: 'nina@usermail.com',
+    firstName: 'TEST ',
+    lastName: 'TEST ',
+    typeName: 'Private-user',
+    phone: 'TEST ',
+    points: 0,
+    countyId: 1,
+    active: 1
+  };
+
+  userDao.addUser(post,'hashedpassword..', callback);
+});
+
+test("check if nina@usermail.com is added as user", done => {
+  function callback(status, data) {
+    console.log(
+      "Test callback: status=" + status + ", data=" + JSON.stringify(data)
+    );
+    expect(data[0].mail).toBe('nina@usermail.com');
+    done();
+  }
+  userDao.getUser('nina@usermail.com', callback);
+});
+
+//EVENT-TESTING
+//-----------------------------------------------------------------
+
+//COUNTY-TESTING
+//-----------------------------------------------------------------
+
+
+//CATEGORY-TESTING
+//-----------------------------------------------------------------
--- server/tests/sqlFiles/createTestData.sql
+++ server/tests/sqlFiles/createTestData.sql
@@ -1,20 +1,49 @@
-INSERT INTO user (mail, firstName, lastName, typeName, phone, password, points, countyId, active)
-VALUES ('halo@gutta.no', 'per', 'sandberg', 'privat', '34985687', 'hei123', 0, 1, 1);
-INSERT INTO user (mail, firstName, lastName, typeName, phone, password, points, countyId, active)
-VALUES ('1@gutta.no', 'per', 'sandberg', 'privat', '34985687', 'hei123', 0, 1, 1);
-INSERT INTO user (mail, firstName, lastName, typeName, phone, password, points, countyId, active)
-VALUES ('halo2@gutta.no', 'per', 'sandberg', 'privat', '34985687', 'hei123', 0, 1, 1);
-INSERT INTO user (mail, firstName, lastName, typeName, phone, password, points, countyId, active)
-VALUES ('halo3@gutta.no', 'per', 'sandberg', 'privat', '34985687', 'hei123', 0, 1, 1);
-INSERT INTO user (mail, firstName, lastName, typeName, phone, password, points, countyId, active)
-VALUES ('halo4@gutta.no', 'per', 'sandberg', 'privat', '34985687', 'hei123', 0, 1, 1);
-INSERT INTO user (mail, firstName, lastName, typeName, phone, password, points, countyId, active)
-VALUES ('halo5@gutta.no', 'per', 'sandberg', 'privat', '34985687', 'hei123', 0, 1, 1);
-INSERT INTO user (mail, firstName, lastName, typeName, phone, password, points, countyId, active)
-VALUES ('halo6@gutta.no', 'per', 'sandberg', 'privat', '34985687', 'hei123', 0, 1, 1);
-INSERT INTO user (mail, firstName, lastName, typeName, phone, password, points, countyId, active)
-VALUES ('halo7@gutta.no', 'per', 'sandberg', 'privat', '34985687', 'hei123', 0, 1, 1);
-INSERT INTO user (mail, firstName, lastName, typeName, phone, password, points, countyId, active)
-VALUES ('halo8@gutta.no', 'per', 'sandberg', 'privat', '34985687', 'hei123', 0, 1, 1);
-INSERT INTO user (mail, firstName, lastName, typeName, phone, password, points, countyId, active)
-VALUES ('halo9@gutta.no', 'per', 'sandberg', 'privat', '34985687', 'hei123', 0, 1, 1);
+Insert into aadneny.category(categoryId, name, priority, active) values (1,'Strøm',1,1);
+Insert into aadneny.category(categoryId, name, priority, active) values (2,'Vann og avløp',2,1);
+
+Insert into aadneny.category2(category2Id,categoryId, name, active) values (1,1,'Strømbrudd',1);
+Insert into aadneny.category2(category2Id,categoryId, name, active) values (2,2,'Vannstopp',1);
+
+Insert into aadneny.category3(category3Id,category2Id, name, active) values (1,2,'Vannmangel',1);
+
+Insert into aadneny.types(typeName, active) values('Company', 1);
+Insert into aadneny.types(typeName, active) values('Admin', 1);
+Insert into aadneny.types(typeName, active) values('Employee', 1);
+Insert into aadneny.types(typeName, active) values('Private-user', 1);
+
+
+Insert into aadneny.county(name, active) values ('Oslo', 1);
+Insert into aadneny.county(name, active) values('Trondheim', 1);
+Insert into aadneny.county(name, active) values('Kristiansand', 1);
+
+
+Insert into aadneny.status(statusName) values('Registered');
+Insert into aadneny.status(statusName) values('In progress');
+Insert into aadneny.status(statusName) values('Completed');
+
+Insert into aadneny.company(companyMail, companyName, firstName, lastName,adresse, postnr, password, phone, description, orgNumber) values ('company1@company.com','Company1','Ola','Pettersen','Adresse1', 2020, 'fghj','123456','this is a company',1234);
+Insert into aadneny.company(companyMail, companyName, firstName, lastName, adresse, postnr, password, phone, description, orgNumber) values ('company2@company.com','Company2','Ola','Pettersen','Adresse2',4040, 'fghj','123456','a description',3456);
+
+Insert into aadneny.companyCategories(companyMail, categoryId) values ('company1@company.com',1);
+Insert into aadneny.companyCategories(companyMail, categoryId) values ('company2@company.com',2);
+
+Insert into aadneny.user(mail, firstName, lastName, password, typeName, phone, points, countyId, active) values ('ola@usermail.com', 'Ola', 'Nordman','password1' , 'Admin', 97723342, 0, 1,1);
+Insert into aadneny.user(mail, firstName, lastName, password, typeName, phone, points, countyId, active) values ('kari@usermail.com', 'Kari', 'Olsen', 'password2' , 'Admin', 97766641, 0, 2,1);
+
+Insert into aadneny.issues(issueId, userMail, latitude, longitude,address, text, pic, date, statusName, categoryId, categoryLevel,countyId, active) values (1, 'ola@usermail.com', 23.432, 43.12,'Dette er en adresse', 'en tekst', 'picture', '2001-01-01', 'In progress', 1, 1, 1, 1);
+Insert into aadneny.issues(issueId, userMail, latitude, longitude,address, text, pic, date, statusName, categoryId,categoryLevel, countyId, active) values (2, 'kari@usermail.com', 73.432, 54.12,'Dette er en adresse2', 'this is a text', 'picture2', '2019-01-01', 'Registered', 1, 1, 1, 1);
+
+Insert into aadneny.companyIssues(issueId, companyMail) values (1 ,'company1@company.com');
+Insert into aadneny.companyIssues(issueId, companyMail) values (2 ,'company2@company.com');
+
+Insert into aadneny.event(title, text, latitude, longitude, date, userMail, countyId, active) values ('Veiarbeid i midtbyen', 'Veiarbeid i midtbyen torsdag kveld', 0, 0, '10.01.2019', 'ola@usermail.com', 2, 1);
+Insert into aadneny.event(title, text, latitude, longitude, date, userMail, countyId, active) values ('Konsert', 'Konsert kan skape kø ut av byen mellom 20.00 og 23.00', 0, 0, '13.01.2019', 'kari@usermail.com', 3, 1);
+
+Insert into aadneny.pushAlerts(countyId, categoryId, userMail) values(1, 1, 'kari@usermail.com');
+
+
+Insert into aadneny.userCounties(userMail, countyId) values('ola@usermail.com', 1);
+Insert into aadneny.userCounties(userMail, countyId) values('ola@usermail.com', 2);
+Insert into aadneny.userCounties(userMail, countyId) values('kari@usermail.com', 1);
+
+
