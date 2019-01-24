e131dc8e3d878295bc154952fb369ace70b35aa6
quannt <quannt@stud.ntnu.no>
2019-01-24 14:38:47 +0100

    Moved filterAll and filterMine to Filter component

--- client/src/views/MinSide/countySubscription/countySubscription.js
+++ client/src/views/MinSide/countySubscription/countySubscription.js
@@ -196,46 +196,4 @@ export class CountySubscription extends Component<Props, State> {
 
     );
   }
-
-    filterAll() {
-        // Declare variables
-        var input, filter, ul, li, a, i, txtValue;
-        input = document.getElementById('allCounties');
-        filter = input.value.toUpperCase();
-        ul = document.getElementById("allCountiesList");
-        li = ul.getElementsByTagName("li");
-
-        // Loop through all list items, and hide those who don't match the search query
-        for (i = 0; i < li.length; i++) {
-            a = li[i].getElementsByTagName("a")[0];
-            txtValue = a.textContent || a.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                li[i].style.display = "";
-            } else {
-                li[i].style.display = "none";
-            }
-        }
-
-    }
-
-    filterMine() {
-        // Declare variables
-        var input, filter, ul, li, a, i, txtValue;
-        input = document.getElementById('myCounties');
-        filter = input.value.toUpperCase();
-        ul = document.getElementById("myCountiesList");
-        li = ul.getElementsByTagName("li");
-
-        // Loop through all list items, and hide those who don't match the search query
-        for (i = 0; i < li.length; i++) {
-            a = li[i].getElementsByTagName("a")[0];
-            txtValue = a.textContent || a.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                li[i].style.display = "";
-            } else {
-                li[i].style.display = "none";
-            }
-        }
-    }
-
 }
