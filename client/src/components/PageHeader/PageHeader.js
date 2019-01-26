fd8f54c034c6e9e0c42839cb86b7b3a7ac602d8a
aadneny <aadneny@stud.ntnu.iie.no>
2019-01-26 14:26:45 +0100

    Merge branch 'master' of https://gitlab.stud.idi.ntnu.no/sindretn/hverdagshelt
    # Conflicts:
    #	client/src/components/AdminAddCategory/adminAddCategory.js
    #	client/src/components/ChooseCategory/ChooseCategory.js
    #	client/src/components/registeradmin/registeradmin.js
    #	client/src/services.js

--- client/src/components/PageHeader/PageHeader.js
+++ client/src/components/PageHeader/PageHeader.js
@@ -6,7 +6,7 @@ export class PageHeader extends Component<{ title: string }> {
   render() {
     return (
       <div align="center">
-        <h3>{this.props.title}</h3>
+        <h3 >{this.props.title}</h3>
         <hr/>
       </div>
     );
