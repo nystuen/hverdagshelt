a06c2213e5f7c53e27e4b05865d7367904633bdd
Magnus Revheim Martinsen <magnusrm.stud.ntnu.no>
2019-01-27 23:12:22 +0100

    wrong routing

--- client/src/views/admin/registerEvent/registerEventForm/EventFormFirstPage.js
+++ client/src/views/admin/registerEvent/registerEventForm/EventFormFirstPage.js
@@ -204,7 +204,7 @@ export class EventFormFirstPage extends Component<{}, State> {
                         className="next + ' ' + submitButton"
                         onClick={this.handleSubmit}
                       >
-                        Meld feil <Glyphicon glyph="glyphicon glyphicon-arrow-right"/>
+                        Meld hendelse <Glyphicon glyph="glyphicon glyphicon-arrow-right"/>
                       </Button>
                     </form>
                   </div>
